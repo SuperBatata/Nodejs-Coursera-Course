017b507501b1c8fb83667f7198fdb28a50461259
SiBatata <khaled.battiche@esprit.tn>
2020-11-24 10:08:44 +0100

    assigment 4

--- models/user.js
+++ models/user.js
@@ -2,21 +2,27 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-var User = new Schema({
+var userSchema = new Schema({
+    username: String,
+    password: String,
     firstname: {
-        type: String,
+      type: String,
         default: ''
     },
     lastname: {
-        type: String,
+      type: String,
         default: ''
     },
-    admin: {
+    admin:   {
         type: Boolean,
         default: false
     }
 });
 
-User.plugin(passportLocalMongoose);
+userSchema.methods.getName = function() {
+    return (this.firstname + ' ' + this.lastname);
+};
 
-module.exports = mongoose.model('User', User);
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
